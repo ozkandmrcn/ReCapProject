85154367ae245c503c898ca543577d14519691b3
ozkandemiircan@gmail.com <ozkandemiircan@gmail.com>
2021-01-31 18:28:29 +0300

    Add project files.

---
+++ Business/Concrete/BrandManager.cs
@@ -0,0 +1,60 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public void Add(Brand brand)
+        {
+            if (brand.BrandName.Length > 2)
+            {
+                _brandDal.Add(brand);
+                Console.WriteLine("Marka sisteme basariyla eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lutfen marka ismini en az iki karakter olacak sekilde giriniz.");
+            }
+        }
+
+        public void Delete(Brand brand)
+        {
+            _brandDal.Delete(brand);
+            Console.WriteLine("Marka sistemden basariyla silindi.");
+        }
+
+        public List<Brand> GetAll()
+        {
+            return _brandDal.GetAll();
+        }
+
+        public Brand GetById(int id)
+        {
+            return _brandDal.Get();
+        }
+
+        public void Update(Brand brand)
+        {
+            if (brand.BrandName.Length > 2)
+            {
+                _brandDal.Update(brand);
+                Console.WriteLine("Marka sistemde basariyla guncellendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lutfen marka ismini en az iki karakter olacak sekilde giriniz.");
+            }
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,48 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using DataAccess.Concrete.InMemory;
+
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            Car car = new Car() { Id = 8, BrandId = 2, ColorId = 3, DailyPrice = 10000, Description = "Aile Arabası", ModelYear = "2018" };
+            Car car1 = new Car() { Id = 1, BrandId = 2, ColorId = 3, DailyPrice = 111000, Description = "Temiz", ModelYear = "2020" };
+
+            carManager.Add(car);
+            carManager.Delete(car);
+            carManager.GetAll();
+            carManager.GetById(1);
+            carManager.Update(car1);
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine("Id : "+item.Id);
+                Console.WriteLine("MarkaId :"+item.BrandId);
+                Console.WriteLine("RenkId :"+item.ColorId);
+
+                Console.WriteLine("Fiyat :"+item.DailyPrice);
+                Console.WriteLine("Acıklama :"+item.Description);
+                Console.WriteLine("Model:"+item.ModelYear);
+
+                Console.WriteLine("--------------------");
+
+
+            }
+           
+           
+            
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
